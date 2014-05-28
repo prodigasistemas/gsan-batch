c34fadf775c5e5a23d5c14220e9b0b232850a170
Marcelo de Brito Garcia <mbgarcia_pa@yahoo.com.br>
2014-05-28 17:14:48 -0300

    ok

--- src/main/java/br/gov/batch/gerardadosleitura/GerarDadosLeitura.java
+++ src/main/java/br/gov/batch/gerardadosleitura/GerarDadosLeitura.java
@@ -3,27 +3,37 @@ package br.gov.batch.gerardadosleitura;
 import javax.batch.api.chunk.ItemProcessor;
 import javax.inject.Named;
 
+import org.jboss.logging.Logger;
+
+import br.gov.model.Status;
 import br.gov.model.cadastro.Imovel;
 
 @Named
 public class GerarDadosLeitura implements ItemProcessor {
+	private static Logger logger = Logger.getLogger(GerarDadosLeitura.class);
 	
 	public GerarDadosLeitura() {
 	}
 
     public Imovel processItem(Object param) {
-    	ImovelPreFaturamento i = (ImovelPreFaturamento) param;
-    	
-    	Imovel imovel = new Imovel();
+    	Imovel imovel = (Imovel) param;
     	
     	boolean temHidrometro = temHidrometro(imovel);
+    	
+    	boolean situacaoFaturamento = situacaoFaturamento(imovel); 
+    	
+    	logger.info("Imovel: " + imovel.getId() + " temHidrometro: " + temHidrometro + " situacao: " + situacaoFaturamento);
     	
         return imovel;
     }
 
 	protected boolean temHidrometro(Imovel imovel) {
 		return imovel.getLigacaoAgua() != null 
-				&& imovel.getLigacaoAgua().getHidrometroInstalacaoHistorico() != null 
-				&& imovel.getLigacaoAgua().getHidrometroInstalacaoHistorico().getId() != null;
+				&& imovel.getLigacaoAgua().getHidrometroInstalacaoHistorico() != null; 
+	}
+	
+	private boolean situacaoFaturamento(Imovel imovel) {
+		return imovel.getLigacaoAguaSituacao().getSituacaoFaturamento() == Status.ATIVO ||
+			imovel.getLigacaoEsgotoSituacao().getSituacaoFaturamento() == Status.ATIVO;
 	}
 }
