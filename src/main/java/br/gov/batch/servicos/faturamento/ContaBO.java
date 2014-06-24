de71fe73cc8b66f1917da265da15b83ce95ec0a3
Marcelo de Brito Garcia <mbgarcia_pa@yahoo.com.br>
2014-06-24 18:11:03 -0300

    #6 - Determinar data de vencimento de conta

--- src/main/java/br/gov/batch/servicos/faturamento/ContaBO.java
+++ src/main/java/br/gov/batch/servicos/faturamento/ContaBO.java
@@ -5,8 +5,12 @@ import java.util.Date;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
+import br.gov.model.Status;
 import br.gov.model.cadastro.Cliente;
 import br.gov.model.cadastro.Imovel;
+import br.gov.model.cadastro.ImovelContaEnvio;
+import br.gov.model.cadastro.SistemaParametros;
+import br.gov.model.faturamento.Conta;
 import br.gov.model.util.Utilitarios;
 import br.gov.servicos.cadastro.ClienteRepositorio;
 import br.gov.servicos.to.ContaTO;
@@ -17,39 +21,56 @@ public class ContaBO {
 	@EJB
 	private ClienteRepositorio clienteRepositorio;
 	
-	public Date determinarVencimentoConta(Imovel imovel){
-		ContaTO contaTO = new ContaTO();
-		contaTO = vencimentoAlternativo(contaTO);
+	@EJB
+	private SistemaParametros sistemaParametros;
+	
+	public Conta gerarConta(Imovel imovel, Date dataVencimentoRota){
+		Conta.Builder builder = new Conta.Builder();
+		builder.dataVencimentoConta(this.determinarVencimentoConta(imovel, dataVencimentoRota));
+		Conta conta = builder.build();
+		
+		return conta;
+	}
+	
+	public Date determinarVencimentoConta(Imovel imovel, Date dataVencimentoRota){
+		ContaTO contaTO = vencimentoAlternativo(imovel, dataVencimentoRota);
 		
 		if (contaTO.comVencimentoAlternativo()){
 			if (contaTO.vencimentoMesSeguinte()){
 				contaTO.adicionaMesAoVencimento();
-			}else{
-				contaTO.ajustaDiaVencimentoAlternativo();
+			}else if (dataVencimentoRota.after(contaTO.getDataVencimentoConta())){
+				Date dataAtualMaisDiasMinimoEmissao = Utilitarios.adicionarDias(new Date(),	sistemaParametros.getNumeroMinimoDiasEmissaoVencimento());
 				
-				if (contaTO.vencimentoRotaSuperiorAoAlternativo()){
-					Date dataAtualMaisDiasMinimoEmissao = Utilitarios.adicionarDias(new Date(),	contaTO.getNumeroMinimoDiasEmissaoVencimento());
-					
-					
+				if (dataAtualMaisDiasMinimoEmissao.after(contaTO.getDataVencimentoConta())){
+					contaTO.adicionaMesAoVencimento();
 				}
 			}
 		}
-		return null;
+		
+		if ((imovel.getImovelContaEnvio() == ImovelContaEnvio.ENVIAR_CLIENTE_RESPONSAVEL || imovel.getImovelContaEnvio() == ImovelContaEnvio.NAO_PAGAVEL_IMOVEL_PAGAVEL_RESPONSAVEL)
+				&& !contaTO.comVencimentoAlternativo()
+				&& imovel.getIndicadorDebitoConta() == Status.INATIVO) {
+			contaTO.adicionaDiasAoVencimento(sistemaParametros.getNumeroDiasAdicionaisCorreios());
+		}
+
+		return contaTO.getDataVencimentoConta();
 	}
 	
-	public ContaTO vencimentoAlternativo(ContaTO contaTO){
-		if (contaTO.getImovel().existeDiaVencimento() && !contaTO.getImovel().emissaoExtratoFaturamento()) {
-			contaTO.setDiaVencimentoAlternativo(contaTO.getImovel().getDiaVencimento());
-			contaTO.setIndicadorVencimentoMesSeguinte(contaTO.getImovel().getIndicadorVencimentoMesSeguinte());
+	public ContaTO vencimentoAlternativo(Imovel imovel, Date dataVencimentoRota){
+		ContaTO contaTO = new ContaTO();
+		contaTO.setDataVencimentoConta(dataVencimentoRota);
+		if (imovel.existeDiaVencimento() && !imovel.emissaoExtratoFaturamento()) {
+			contaTO.setDiaVencimentoAlternativo(imovel.getDiaVencimento());
+			contaTO.setIndicadorVencimentoMesSeguinte(imovel.getIndicadorVencimentoMesSeguinte());
 		} else {
-			Cliente cliente = clienteRepositorio.buscarClienteResponsavelPorImovel(contaTO.getImovel().getId());
+			Cliente cliente = clienteRepositorio.buscarClienteResponsavelPorImovel(imovel.getId());
 
 			if (cliente != null) {
 				if (cliente.existeDiaVencimento()) {
 					contaTO.setDiaVencimentoAlternativo(cliente.getDiaVencimento());
 					contaTO.setIndicadorVencimentoMesSeguinte(cliente.getIndicadorVencimentoMesSeguinte());
 				}
-				else if (contaTO.getImovel().emissaoExtratoFaturamento()) {
+				else if (imovel.emissaoExtratoFaturamento()) {
 					contaTO.setDiaVencimentoAlternativo(Utilitarios.obterUltimoDiaMes(contaTO.getDataVencimentoConta()));
 				}
 			}
---
+++ src/test/java/br/gov/batch/servicos/faturamento/ContaBOTest.java
@@ -0,0 +1,214 @@
+package br.gov.batch.servicos.faturamento;
+
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.junit.Assert.assertEquals;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.easymock.EasyMockRunner;
+import org.easymock.IExpectationSetters;
+import org.easymock.Mock;
+import org.easymock.TestSubject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import br.gov.model.Status;
+import br.gov.model.cadastro.Cliente;
+import br.gov.model.cadastro.Imovel;
+import br.gov.model.cadastro.ImovelContaEnvio;
+import br.gov.model.cadastro.SistemaParametros;
+import br.gov.servicos.cadastro.ClienteRepositorio;
+
+@RunWith(EasyMockRunner.class)
+public class ContaBOTest {
+	
+	@TestSubject
+	private ContaBO contaBO;
+	
+	@Mock
+	private ClienteRepositorio clienteRepositorioMock;
+	
+	@Mock
+	private SistemaParametros sistemaParametrosMock;
+	
+	private String textoDia10MesSeguinte;
+	
+	private Date dia15MesSeguinte;
+	
+	private DateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+	
+	private Imovel imovel;
+	
+	@Before
+	public void setUp(){
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date());
+		cal.add(Calendar.MONTH, 1);
+		cal.set(Calendar.DAY_OF_MONTH, 15);
+		dia15MesSeguinte = cal.getTime();
+		textoDia10MesSeguinte = format.format(cal.getTime());
+		
+		imovel = new Imovel();
+		imovel.setId(1L);
+		
+		
+		contaBO = new ContaBO();
+	}
+	
+	private void replayAll() {
+		replay(clienteRepositorioMock);
+		replay(sistemaParametrosMock);
+	}
+	
+	private IExpectationSetters<Cliente> mountClienteRepositorioMock() {
+		return expect(clienteRepositorioMock.buscarClienteResponsavelPorImovel(1L));
+	}
+	
+	private IExpectationSetters<Short> mountSistemaParametrosRepositorioMock() {
+		return expect(sistemaParametrosMock.getNumeroMinimoDiasEmissaoVencimento());
+	}
+	
+	private IExpectationSetters<Short> mountSistemaParametrosRepositorioDiasCorreiosMock() {
+		return expect(sistemaParametrosMock.getNumeroDiasAdicionaisCorreios());
+	}
+	
+	@Test
+	public void semVencimentoAlternativo(){
+		mountClienteRepositorioMock().andReturn(null);
+		mountSistemaParametrosRepositorioMock().andReturn((short) 10);
+		replayAll();
+		
+		assertEquals(textoDia10MesSeguinte, format.format(contaBO.determinarVencimentoConta(imovel, dia15MesSeguinte)));
+	}
+	
+	@Test
+	public void vencimentoAlternativoImovelAntesVencerRota(){
+		imovel.setDiaVencimento((short) 5);
+		mountClienteRepositorioMock().andReturn(null);
+		mountSistemaParametrosRepositorioMock().andReturn((short) 10);
+		replayAll();
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(dia15MesSeguinte);
+		cal.set(Calendar.DAY_OF_MONTH, 5);
+		
+		assertEquals(format.format(cal.getTime()), format.format(contaBO.determinarVencimentoConta(imovel, dia15MesSeguinte)));
+	}
+	
+	@Test
+	public void vencimentoAlternativoImovelAposVencerRota(){
+		imovel.setDiaVencimento((short) 25);
+		mountClienteRepositorioMock().andReturn(null);
+		mountSistemaParametrosRepositorioMock().andReturn((short) 10);
+		replayAll();
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(dia15MesSeguinte);
+		cal.set(Calendar.DAY_OF_MONTH, 25);
+		
+		assertEquals(format.format(cal.getTime()), format.format(contaBO.determinarVencimentoConta(imovel, dia15MesSeguinte)));
+	}
+
+	@Test
+	public void vencimentoAlternativoImovelComDiasParaEmissaoSuperiorAoVencimento(){
+		imovel.setDiaVencimento((short) 5);
+		mountClienteRepositorioMock().andReturn(null);
+		mountSistemaParametrosRepositorioMock().andReturn((short) 60);
+		replayAll();
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(dia15MesSeguinte);
+		cal.set(Calendar.DAY_OF_MONTH, 5);
+		cal.add(Calendar.MONTH, 1);
+		
+		assertEquals(format.format(cal.getTime()), format.format(contaBO.determinarVencimentoConta(imovel, dia15MesSeguinte)));
+	}
+	
+	@Test
+	public void vencimentoAlternativcoImovelComExtratoFaturamentoVencimentoMesSeguinte(){
+		imovel.setDiaVencimento((short) 5);
+		imovel.setIndicadorEmissaoExtratoFaturamento((short) 2);
+		imovel.setIndicadorVencimentoMesSeguinte((short) 1);
+		mountClienteRepositorioMock().andReturn(null);
+		mountSistemaParametrosRepositorioMock().andReturn((short) 10);
+		replayAll();
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(dia15MesSeguinte);
+		cal.set(Calendar.DAY_OF_MONTH, 5);
+		cal.add(Calendar.MONTH, 1);
+		
+		assertEquals(format.format(cal.getTime()), format.format(contaBO.determinarVencimentoConta(imovel, dia15MesSeguinte)));
+	}
+		
+	@Test
+	public void vencimentoAlternativoCliente(){
+		Cliente cliente = new Cliente();
+		cliente.setDiaVencimento((short) 5);
+		mountClienteRepositorioMock().andReturn(cliente);
+		mountSistemaParametrosRepositorioMock().andReturn((short) 10);
+		replayAll();
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(dia15MesSeguinte);
+		cal.set(Calendar.DAY_OF_MONTH, 5);
+		
+		assertEquals(format.format(cal.getTime()), format.format(contaBO.determinarVencimentoConta(imovel, dia15MesSeguinte)));
+	}
+	
+	@Test
+	public void vencimentoAlternativoClienteMesSeguinte(){
+		Cliente cliente = new Cliente();
+		cliente.setDiaVencimento((short) 5);
+		cliente.setIndicadorVencimentoMesSeguinte((short) 1);
+		mountClienteRepositorioMock().andReturn(cliente);
+		mountSistemaParametrosRepositorioMock().andReturn((short) 10);
+		replayAll();
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(dia15MesSeguinte);
+		cal.set(Calendar.DAY_OF_MONTH, 5);
+		cal.add(Calendar.MONTH, 1);
+		
+		assertEquals(format.format(cal.getTime()), format.format(contaBO.determinarVencimentoConta(imovel, dia15MesSeguinte)));
+	}
+	
+	@Test
+	public void vencimentoAlternativoImovelPorEmissaoExtratoFaturamento(){
+		Cliente cliente = new Cliente();
+		imovel.setIndicadorEmissaoExtratoFaturamento((short) 1);
+		mountClienteRepositorioMock().andReturn(cliente);
+		mountSistemaParametrosRepositorioMock().andReturn((short) 10);
+		replayAll();
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(dia15MesSeguinte);
+		cal.set(Calendar.DAY_OF_MONTH, cal.getMaximum(Calendar.DAY_OF_MONTH));
+		
+		assertEquals(format.format(cal.getTime()), format.format(contaBO.determinarVencimentoConta(imovel, dia15MesSeguinte)));
+	}
+	
+	@Test
+	public void semVencimentoAlternativoDebitoEmContaEEnvioPelosCorreios(){
+		Cliente cliente = new Cliente();
+		imovel.setImovelContaEnvio(ImovelContaEnvio.ENVIAR_CLIENTE_RESPONSAVEL);
+		imovel.setIndicadorDebitoConta(Status.INATIVO);
+		
+		mountClienteRepositorioMock().andReturn(cliente);
+		mountSistemaParametrosRepositorioMock().andReturn((short) 10);
+		mountSistemaParametrosRepositorioDiasCorreiosMock().andReturn((short) 10);
+		replayAll();
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(dia15MesSeguinte);
+		cal.add(Calendar.DAY_OF_MONTH, 10);
+		
+		assertEquals(format.format(cal.getTime()), format.format(contaBO.determinarVencimentoConta(imovel, dia15MesSeguinte)));
+	}
+	
+}
