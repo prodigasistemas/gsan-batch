b47f1d3f369e2c72ca9cccd15e5b999f06b6a497
Paulo Igor <piagodinho@gmail.com>
2014-09-01 11:20:24 -0300

    Retirando o atributo imovel

--- src/main/java/br/gov/batch/servicos/faturamento/arquivo/ArquivoTextoTipo02.java
+++ src/main/java/br/gov/batch/servicos/faturamento/arquivo/ArquivoTextoTipo02.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 import br.gov.model.cadastro.ICategoria;
 import br.gov.model.cadastro.Imovel;
@@ -14,38 +15,33 @@ import br.gov.servicos.cadastro.ImovelSubcategoriaRepositorio;
 @Stateless
 public class ArquivoTextoTipo02 {
 	
-	@EJB
+	@Inject
 	private SistemaParametros sistemaParametro;
 
 	@EJB
 	private ImovelSubcategoriaRepositorio imovelSubcategoriaRepositorio;
 
-	private Imovel imovel;
 	private StringBuilder builder;
 	private final String TIPO_REGISTRO = "02";
 
 	public String build(Imovel imovel){
-		this.imovel = imovel;
 		builder = new StringBuilder();
 
 		int quantidadeLinhas = 0;
 
 		Collection<ICategoria> colecaoCategorias = imovelSubcategoriaRepositorio.buscarQuantidadeEconomiasPorImovel(imovel.getId());
-
-		if (colecaoCategorias != null && !colecaoCategorias.isEmpty()) {
-			for (ICategoria categoria : colecaoCategorias) {
-				quantidadeLinhas = quantidadeLinhas + 1;
-				
-				buildTipo02(categoria);
-			}
+		
+		for (ICategoria categoria : colecaoCategorias) {
+			quantidadeLinhas = quantidadeLinhas + 1;
+			buildTipo02(categoria, imovel.getId());
 		}
 		
 		return builder.toString();
 	}
 	
-	private void buildTipo02(ICategoria iCategoria) {
+	private void buildTipo02(ICategoria iCategoria, Integer idImovel) {
 		builder.append(TIPO_REGISTRO);
-		builder.append(Utilitarios.completaComZerosEsquerda(9, imovel.getId().toString()));
+		builder.append(Utilitarios.completaComZerosEsquerda(9, idImovel.toString()));
 		builder.append(getCodigoCategoriaOuSubcategoria(iCategoria));
 		builder.append(getDescricaoCategoriaOuSubcategoria(iCategoria));
 		builder.append(getCodigoSubcategoria(iCategoria));
@@ -75,7 +71,7 @@ public class ArquivoTextoTipo02 {
 	
 	private String getSubcategoriaDescricaoAbreviada(ICategoria iCategoria) {
 		if (sistemaParametro.indicadorTarifaCategoria()) {
-			return Utilitarios.completaTexto(50, iCategoria.getDescricaoAbreviada());
+			return Utilitarios.completaTexto(3, iCategoria.getDescricaoAbreviada());
 		} else {
 			return Utilitarios.completaTexto(3, iCategoria.getCategoria().getDescricaoAbreviada());
 		}
