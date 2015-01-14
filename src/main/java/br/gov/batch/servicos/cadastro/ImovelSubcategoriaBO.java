0ef4f10bacfe5b01c97b918a3e672201361827ae
Marcelo de Brito Garcia <mbgarcia_pa@yahoo.com.br>
2015-01-14 15:14:44 -0300

    Escrevendo linha 01 ate sequencial da rota

---
+++ src/main/java/br/gov/batch/servicos/cadastro/ImovelSubcategoriaBO.java
@@ -0,0 +1,33 @@
+package br.gov.batch.servicos.cadastro;
+
+import java.util.Collection;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+import br.gov.model.cadastro.ICategoria;
+import br.gov.servicos.cadastro.ImovelSubcategoriaRepositorio;
+
+@Stateless
+public class ImovelSubcategoriaBO {
+
+    @EJB
+    private ImovelSubcategoriaRepositorio imovelSubcategoriaRepositorio; 
+
+    public Integer buscaIdCategoriaComMaisEconomias(Integer idImovel){
+        Collection<ICategoria> categorias = imovelSubcategoriaRepositorio.buscarQuantidadeEconomiasCategoria(idImovel);
+        
+        int idCategoria = 0;
+        
+        int qtdEconomias = 0;
+        
+        for (ICategoria c : categorias) {
+            if (qtdEconomias < c.getQuantidadeEconomias()){
+                qtdEconomias =  c.getQuantidadeEconomias();
+                idCategoria = c.getId();
+            }
+        }
+        
+        return idCategoria;
+    }
+}
---
+++ src/main/java/br/gov/batch/servicos/faturamento/VolumeMedioAguaEsgotoTO.java
@@ -0,0 +1,36 @@
+package br.gov.batch.servicos.faturamento;
+
+import java.io.Serializable;
+
+public class VolumeMedioAguaEsgotoTO implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3360457013333261380L;
+
+	private Integer consumoMedio;
+	private Integer quantidadeMesesConsiderados;
+	
+	public VolumeMedioAguaEsgotoTO(Integer consumoMedio,
+			Integer quantidadeMesesConsiderados) {
+		super();
+		this.consumoMedio = consumoMedio;
+		this.quantidadeMesesConsiderados = quantidadeMesesConsiderados;
+	}
+	
+	public Integer getConsumoMedio() {
+		return consumoMedio;
+	}
+	public void setConsumoMedio(Integer consumoMedio) {
+		this.consumoMedio = consumoMedio;
+	}
+	public Integer getQuantidadeMesesConsiderados() {
+		return quantidadeMesesConsiderados;
+	}
+	public void setQuantidadeMesesConsiderados(Integer quantidadeMesesConsiderados) {
+		this.quantidadeMesesConsiderados = quantidadeMesesConsiderados;
+	}
+	
+	
+}
