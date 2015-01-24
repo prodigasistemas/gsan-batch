31bae6ef24651e976a1c54c3b46f7fbdd8956907
Pamela Gatinho <pamelagatinho@gmail.com>
2015-01-24 08:28:49 -0300

    Merge

--- src/main/java/br/gov/batch/servicos/faturamento/arquivo/ArquivoTextoTipo01DadosConta.java
+++ src/main/java/br/gov/batch/servicos/faturamento/arquivo/ArquivoTextoTipo01DadosConta.java
@@ -140,8 +140,7 @@ public class ArquivoTextoTipo01DadosConta {
 	private void escreverQualidadeDaAgua() {
     	Integer anoMesReferenciaQualidadeAgua = null;
     	
-		//if (!Boolean.valueOf(repositorioParametros.recuperaPeloNome(NOME_PARAMETRO_FATURAMENTO.EMITIR_CONTA_CODIGO_COMPESA))) {
-    	if (!Boolean.valueOf(repositorioParametros.recuperaPeloNome(NOME_PARAMETRO_FATURAMENTO.ESCREVER_MENSAGEM_CONTA_TRES_PARTES))) {
+		if (!Boolean.valueOf(repositorioParametros.recuperaPeloNome(NOME_PARAMETRO_FATURAMENTO.REFERENCIA_ANTERIOR_PARA_QUALIDADE_AGUA))) {
 			anoMesReferenciaQualidadeAgua = Utilitarios.reduzirMeses(faturamentoGrupo.getAnoMesReferencia(), 1);
 		} else {
 			anoMesReferenciaQualidadeAgua = faturamentoGrupo.getAnoMesReferencia();
