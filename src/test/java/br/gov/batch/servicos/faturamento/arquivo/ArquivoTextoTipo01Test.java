0ff38e08ed66663a3d29bba085c4ae9188d80552
Pamela Gatinho <pamelagatinho@gmail.com>
2015-01-24 08:12:21 -0300

    Refactoring

--- src/test/java/br/gov/batch/servicos/faturamento/arquivo/ArquivoTextoTipo01Test.java
+++ src/test/java/br/gov/batch/servicos/faturamento/arquivo/ArquivoTextoTipo01Test.java
@@ -6,11 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.easymock.EasyMockRunner;
@@ -20,40 +16,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import br.gov.model.Status;
-import br.gov.model.atendimentopublico.LigacaoAgua;
-import br.gov.model.atendimentopublico.LigacaoAguaSituacao;
-import br.gov.model.atendimentopublico.LigacaoEsgotoSituacao;
-import br.gov.model.cadastro.Bairro;
-import br.gov.model.cadastro.Categoria;
-import br.gov.model.cadastro.Cliente;
-import br.gov.model.cadastro.ClienteImovel;
-import br.gov.model.cadastro.ClienteRelacaoTipo;
-import br.gov.model.cadastro.GerenciaRegional;
-import br.gov.model.cadastro.ICategoria;
 import br.gov.model.cadastro.Imovel;
-import br.gov.model.cadastro.ImovelPerfil;
-import br.gov.model.cadastro.Localidade;
-import br.gov.model.cadastro.Logradouro;
-import br.gov.model.cadastro.LogradouroBairro;
-import br.gov.model.cadastro.Quadra;
-import br.gov.model.cadastro.QuadraFace;
-import br.gov.model.cadastro.SetorComercial;
-import br.gov.model.cadastro.Subcategoria;
-import br.gov.model.cadastro.endereco.Cep;
-import br.gov.model.cadastro.endereco.LogradouroCep;
-import br.gov.model.cadastro.endereco.LogradouroTipo;
-import br.gov.model.cadastro.endereco.LogradouroTitulo;
-import br.gov.model.cadastro.endereco.Municipio;
-import br.gov.model.cobranca.CobrancaDocumento;
-import br.gov.model.cobranca.DocumentoTipo;
-import br.gov.model.faturamento.ConsumoTarifa;
-import br.gov.model.faturamento.Conta;
-import br.gov.model.faturamento.FaturamentoGrupo;
-import br.gov.model.faturamento.QualidadeAgua;
-import br.gov.model.faturamento.QualidadeAguaPadrao;
-import br.gov.model.micromedicao.Rota;
-import br.gov.model.operacional.FonteCaptacao;
 
 @RunWith(EasyMockRunner.class)
 public class ArquivoTextoTipo01Test {
@@ -79,57 +42,9 @@ public class ArquivoTextoTipo01Test {
     @Mock
     ArquivoTextoTipo01DadosLocalizacaoImovel dadosLocalizacaoImovelMock;
     
-	private Imovel imovel;
-    private FaturamentoGrupo faturamentoGrupo;
-    private GerenciaRegional gerencia;
-    private Localidade localidade;
-    private ClienteImovel clienteImovelUsuario;
-    private ClienteImovel clienteImovelResponsavel;
-    private Cliente clienteUsuario;
-    private Cliente clienteResponsavel;
-    private Rota rota;
-    private QuadraFace quadraFace;
-    private Conta conta;
-    private LigacaoAguaSituacao ligacaoAguaSituacao;
-    private LigacaoEsgotoSituacao ligacaoEsgotoSituacao;
-    private CobrancaDocumento cobrancaDocumento;
-    
     @Before
     public void init(){
         arquivoTextoTipo01 = new ArquivoTextoTipo01();
-        
-        imovelSetUp();
-        
-        rota = new Rota();
-        rota.setCodigo(Short.valueOf("1"));
-        
-        faturamentoGrupo = new FaturamentoGrupo(1);
-        faturamentoGrupo.setAnoMesReferencia(201501);
-        
-        conta = new Conta();
-        conta.setDataVencimentoConta(new Date());
-        conta.setDataValidadeConta(new Date());
-        conta.setLigacaoAguaSituacao(ligacaoAguaSituacao);
-        conta.setLigacaoEsgotoSituacao(ligacaoEsgotoSituacao);
-        conta.setReferencia(201501);
-        conta.setId(999999999);
-        conta.setDigitoVerificadorConta(Short.valueOf("1"));
-        conta.setFaturamentoGrupo(faturamentoGrupo);
-        
-        cobrancaDocumento = new CobrancaDocumento();
-        cobrancaDocumento.setEmissao(new Date());
-        cobrancaDocumento.setValorDocumento(new BigDecimal(100.00));
-        cobrancaDocumento.setLocalidade(localidade);
-        cobrancaDocumento.setImovel(imovel);
-        cobrancaDocumento.setNumeroSequenciaDocumento(1);
-        cobrancaDocumento.setDocumentoTipo(DocumentoTipo.AVISO_CORTE.getId());
-        
-//        ArquivoTextoTipo01DadosCobranca dadosCobranca = new ArquivoTextoTipo01DadosCobranca(imovel, cobrancaDocumento);
-//        ArquivoTextoTipo01DadosCliente dadosCliente = new ArquivoTextoTipo01DadosCliente(imovel);
-//        ArquivoTextoTipo01DadosConsumo dadosConsumo = new ArquivoTextoTipo01DadosConsumo(imovel, faturamentoGrupo);
-//        ArquivoTextoTipo01DadosConta dadosConta = new ArquivoTextoTipo01DadosConta(imovel, conta);
-//        ArquivoTextoTipo01DadosFaturamento dadosFaturamento = new ArquivoTextoTipo01DadosFaturamento(imovel, conta);
-//        ArquivoTextoTipo01DadosLocalizacaoImovel dadosLocalizacaoImovel = new ArquivoTextoTipo01DadosLocalizacaoImovel(imovel, rota);
         
         arquivoTextoTipo01.setDadosCliente(dadosClienteMock);
         arquivoTextoTipo01.setDadosCobranca(dadosCobrancaMock);
@@ -138,181 +53,7 @@ public class ArquivoTextoTipo01Test {
         arquivoTextoTipo01.setDadosFaturamento(dadosFaturamentoMock);
         arquivoTextoTipo01.setDadosLocalizacaoImovel(dadosLocalizacaoImovelMock);
         
-        arquivoTextoTipo01.setImovel(imovel);
-        arquivoTextoTipo01.setFaturamentoGrupo(faturamentoGrupo);
-        arquivoTextoTipo01.setRota(rota);
-        arquivoTextoTipo01.setConta(conta);
-        arquivoTextoTipo01.setCobrancaDocumento(cobrancaDocumento);
-    }
-    
-    public void imovelSetUp() {
-    	imovel = new Imovel(1234567);
-    	imovel.setIndicadorImovelCondominio(Status.INATIVO.getId());
-    	imovel.setImovelPerfil(new ImovelPerfil(1));
-    	imovel.setLote(Short.valueOf("1234"));
-    	imovel.setSubLote(Short.valueOf("123"));
-    	preencherEnderecoImovel();
-    	
-    	clienteUsuario = new Cliente();
-    	clienteUsuario.setNome("MARIA JOSÉ DA SILVA");
-    	clienteUsuario.setCpf("11111111111");
-    	
-    	clienteResponsavel = new Cliente();
-    	clienteResponsavel.setNome("JOÃO ROBERTO SOUZA");
-    	clienteResponsavel.setCpf("222.222.222-22");
-
-    	clienteImovelUsuario = new ClienteImovel();
-    	clienteImovelUsuario.setCliente(clienteUsuario);
-    	clienteImovelUsuario.setImovel(imovel);
-    	clienteImovelUsuario.setClienteRelacaoTipo(new ClienteRelacaoTipo(ClienteRelacaoTipo.USUARIO));
-
-    	clienteImovelResponsavel = new ClienteImovel();
-    	clienteImovelResponsavel.setCliente(clienteResponsavel);
-    	clienteImovelResponsavel.setImovel(imovel);
-    	clienteImovelResponsavel.setClienteRelacaoTipo(new ClienteRelacaoTipo(ClienteRelacaoTipo.RESPONSAVEL));
-    	
-    	List<ClienteImovel> clientesImovel = new ArrayList<ClienteImovel>();
-    	clientesImovel.add(clienteImovelUsuario);
-    	clientesImovel.add(clienteImovelResponsavel);
-    	imovel.setClienteImoveis(clientesImovel);
-    	
-    	
-    	gerencia = new GerenciaRegional();
-    	gerencia.setNome("BELEM");
-    	
-    	localidade = new Localidade(1);
-    	localidade.setGerenciaRegional(gerencia);
-    	localidade.setDescricao("DESCRICAO DA LOCALIDADE");
-    	localidade.setLogradouroCep(imovel.getLogradouroCep());
-    	localidade.setLogradouroBairro(imovel.getLogradouroBairro());
-    	localidade.setNumeroImovel("10");
-    	localidade.setFone("33224455");
-    	
-    	imovel.setLocalidade(localidade);
-
-    	quadraFace = new QuadraFace(1);
-    	
-    	Quadra quadra = new Quadra(1);
-    	quadra.setNumeroQuadra(1234);
-    	imovel.setQuadra(quadra);
-    	imovel.setQuadraFace(quadraFace);
-    	
-    	imovel.setSetorComercial(new SetorComercial(1));
-    	
-    	ligacaoAguaSituacao = new LigacaoAguaSituacao(LigacaoAguaSituacao.LIGADO);
-    	ligacaoAguaSituacao.setSituacaoFaturamento(Status.ATIVO.getId());
-    	ligacaoAguaSituacao.setIndicadorAbastecimento(Status.ATIVO.getId());
-    	imovel.setLigacaoAguaSituacao(ligacaoAguaSituacao);
-    	
-    	ligacaoEsgotoSituacao = new LigacaoEsgotoSituacao(LigacaoEsgotoSituacao.POTENCIAL);
-    	ligacaoEsgotoSituacao.setSituacaoFaturamento(Status.INATIVO.getId());
-    	imovel.setLigacaoEsgotoSituacao(ligacaoEsgotoSituacao);
-    	
-    	imovel.setConsumoTarifa(new ConsumoTarifa(1));
-    	
-    	imovel.setImovelContaEnvio(1);
-    	
-    	LigacaoAgua ligacaoAgua = new LigacaoAgua();
-    	ligacaoAgua.setConsumoMinimoAgua(10);
-    	imovel.setLigacaoAgua(ligacaoAgua);
-    	imovel.setCodigoDebitoAutomatico(888888888);
-    	
-    	
-    }
-    
-    private List<ICategoria> categoriasSetUp() {
-    	Categoria categoria = new Categoria();
-    	
-    	categoria.setConsumoAlto(50);
-    	categoria.setConsumoEstouro(50);
-    	categoria.setNumeroConsumoMaximoEc(500);
-    	categoria.setMediaBaixoConsumo(30);
-    	categoria.setQuantidadeEconomias(1);
-    	categoria.setVezesMediaAltoConsumo(new BigDecimal("2.0"));
-    	categoria.setVezesMediaEstouro(new BigDecimal("3.0"));
-    	categoria.setPorcentagemMediaBaixoConsumo(new BigDecimal("50.0"));
-    	
-    	List<ICategoria> categorias = new ArrayList<ICategoria>();
-    	categorias.add(categoria);
-    	
-    	return categorias;
-    }
-    
-    private List<ICategoria> subcategoriasSetUp() {
-    	Subcategoria subcategoria = new Subcategoria();
-    	
-    	subcategoria.setIndicadorSazonalidade(Status.INATIVO.getId());
-
-    	List<ICategoria> subcategorias = new ArrayList<ICategoria>();
-    	subcategorias.add(subcategoria);
-    	
-    	return subcategorias;
-    }
-    
-    private String[] obterMensagem() {
-    	String[] mensagemConta = new String[3];
-    	
-    	mensagemConta[0] = "MENSAGEM EM CONTA - 1";
-    	mensagemConta[1] = "MENSAGEM EM CONTA - 2";
-    	mensagemConta[2] = "MENSAGEM EM CONTA - 3";
-    	
-    	return mensagemConta;
-    }
-    
-    private List<QualidadeAguaPadrao> obterQualidadeAguaPadrao() {
-    	QualidadeAguaPadrao qualidade = new QualidadeAguaPadrao();
-    	
-    	List<QualidadeAguaPadrao> qualidades = new ArrayList<QualidadeAguaPadrao>();
-    	
-    	qualidades.add(qualidade);
-    	return qualidades;
-    }
-    
-    private QualidadeAgua obterQualidadeAgua() {
-    	FonteCaptacao fonte = new FonteCaptacao();
-    	
-    	QualidadeAgua qualidade = new QualidadeAgua();
-    	qualidade.setFonteCaptacao(fonte);
-
-    	return qualidade;
-    }
-    
-    private void preencherEnderecoImovel() {
-    	Cep cep = new Cep();
-    	cep.setCodigo(66093906);
-    	
-    	LogradouroCep logradouroCep = new LogradouroCep();
-    	Logradouro logradouro = new Logradouro();
-    	logradouro.setNome("BARROSO");
-    	logradouroCep.setCep(cep);
-    	
-    	LogradouroTipo logradouroTipo = new LogradouroTipo();
-    	logradouroTipo.setDescricaoAbreviada("AV");
-    	logradouroTipo.setDescricao("AVENIDA");
-    	
-    	LogradouroTitulo logradouroTitulo = new LogradouroTitulo();
-    	logradouroTitulo.setDescricaoAbreviada("ALM");
-    	
-    	logradouro.setLogradouroTipo(logradouroTipo);
-    	logradouro.setLogradouroTitulo(logradouroTitulo);
-    	logradouroCep.setLogradouro(logradouro);
-    	
-    	Municipio municipio = new Municipio();
-    	municipio.setId(1);
-    	municipio.setNome("BELEM");
-    	municipio.setDdd(Short.valueOf("091"));
-    	
-    	Bairro bairro = new Bairro();
-    	bairro.setId(1);
-    	bairro.setNome("MARCO");
-    	bairro.setMunicipio(municipio);
-    	
-    	LogradouroBairro logradouroBairro = new LogradouroBairro();
-    	logradouroBairro.setBairro(bairro);
-    	
-    	imovel.setNumeroImovel("25");
-    	imovel.setLogradouroCep(logradouroCep);
-    	imovel.setLogradouroBairro(logradouroBairro);
+        arquivoTextoTipo01.setImovel(new Imovel(1234567));
     }
     
     @Test
