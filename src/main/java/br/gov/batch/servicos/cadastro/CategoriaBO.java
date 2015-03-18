4942be34a209154420a635a2dde56f0ee68b9ee0
Felipe <felipesantos2089@gmail.com>
2015-03-18 11:23:23 -0300

    Implementando testes para CategoriaBO

--- src/main/java/br/gov/batch/servicos/cadastro/CategoriaBO.java
+++ src/main/java/br/gov/batch/servicos/cadastro/CategoriaBO.java
@@ -3,69 +3,51 @@ package br.gov.batch.servicos.cadastro;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 
 import javax.ejb.Stateless;
 
 import br.gov.model.cadastro.Categoria;
-import br.gov.model.cadastro.ICategoria;
 
 @Stateless
 public class CategoriaBO {
 
-	public Collection<BigDecimal> obterValorPorCategoria(Collection<Categoria> colecaoCategorias, BigDecimal valor) {
+	public Collection<BigDecimal> obterValorPorCategoria(Collection<Categoria> categorias, BigDecimal valor) {
 
-		Collection<BigDecimal> colecaoValoresPorCategoria = new ArrayList<BigDecimal>();
-
-		int somatorioQuantidadeEconomiasCadaCategoria = 0;
-		if (colecaoCategorias != null && !colecaoCategorias.isEmpty()) {
-			Iterator<Categoria> iteratorColecaoCategorias = colecaoCategorias.iterator();
-
-			while (iteratorColecaoCategorias.hasNext()) {
-				ICategoria categoria = (ICategoria) iteratorColecaoCategorias.next();
-				somatorioQuantidadeEconomiasCadaCategoria = somatorioQuantidadeEconomiasCadaCategoria + categoria.getQuantidadeEconomias().intValue();
+		int totalEconomias = 0;
+		if (categorias != null && !categorias.isEmpty()) {
+			for (Categoria categoria : categorias) {
+				totalEconomias += categoria.getQuantidadeEconomias().intValue();
 			}
 		}
 
-		BigDecimal fatorMultiplicacao = somatorioQuantidadeEconomiasCadaCategoria > 0 
-				? valor.divide(new BigDecimal(somatorioQuantidadeEconomiasCadaCategoria), 2, BigDecimal.ROUND_DOWN)
-				: BigDecimal.ZERO;
+		BigDecimal fatorMultiplicacao = totalEconomias > 0 ? valor.divide(new BigDecimal(totalEconomias), 2, BigDecimal.ROUND_DOWN) : BigDecimal.ZERO;
 
-		BigDecimal valorPorCategoriaAcumulado = BigDecimal.ZERO;
-
-
-		if (colecaoCategorias != null && !colecaoCategorias.isEmpty()) {
-			Iterator<Categoria> iteratorColecaoCategorias = colecaoCategorias.iterator();
-
-			while (iteratorColecaoCategorias.hasNext()) {
-				ICategoria categoria = (ICategoria) iteratorColecaoCategorias.next();
-
+		BigDecimal valorAcumuladoPorCategoria = BigDecimal.ZERO;
+		Collection<BigDecimal> valoresPorCategoria = new ArrayList<BigDecimal>();
+		if (categorias != null && !categorias.isEmpty()) {
+			for (Categoria categoria : categorias) {
 				BigDecimal valorPorCategoria = fatorMultiplicacao.multiply(new BigDecimal(categoria.getQuantidadeEconomias()));
-
 				BigDecimal valorTruncado = valorPorCategoria.setScale(2, BigDecimal.ROUND_DOWN);
 
-				valorPorCategoriaAcumulado = valorPorCategoriaAcumulado.add(valorTruncado);
-
-				colecaoValoresPorCategoria.add(valorTruncado);
+				valorAcumuladoPorCategoria = valorAcumuladoPorCategoria.add(valorTruncado);
+				valoresPorCategoria.add(valorTruncado);
 			}
 		}
 
-		valorPorCategoriaAcumulado = valorPorCategoriaAcumulado.setScale(7);
-		
-		if (colecaoValoresPorCategoria.iterator().hasNext()){
-			if (valorPorCategoriaAcumulado.setScale(2, BigDecimal.ROUND_HALF_UP).compareTo(valor.setScale(2, BigDecimal.ROUND_HALF_UP)) == -1) {
-				
-				BigDecimal diferenca = valor.subtract(valorPorCategoriaAcumulado).setScale(2, BigDecimal.ROUND_HALF_UP);
-				
-				BigDecimal categoriaPrimeira = (BigDecimal) colecaoValoresPorCategoria.iterator().next();
-				
-				categoriaPrimeira = categoriaPrimeira.add(diferenca);
-				
-				((ArrayList<BigDecimal>) colecaoValoresPorCategoria).set(0, categoriaPrimeira);
+		valorAcumuladoPorCategoria = valorAcumuladoPorCategoria.setScale(7);
+
+		if (valoresPorCategoria.iterator().hasNext()) {
+			if (valorAcumuladoPorCategoria.setScale(2, BigDecimal.ROUND_HALF_UP).compareTo(valor.setScale(2, BigDecimal.ROUND_HALF_UP)) == -1) {
+				BigDecimal diferenca = valor.subtract(valorAcumuladoPorCategoria).setScale(2, BigDecimal.ROUND_HALF_UP);
+
+				BigDecimal primeiraCategoria = (BigDecimal) valoresPorCategoria.iterator().next();
+
+				primeiraCategoria = primeiraCategoria.add(diferenca);
+
+				((ArrayList<BigDecimal>) valoresPorCategoria).set(0, primeiraCategoria);
 			}
 		}
 
-
-		return colecaoValoresPorCategoria;
+		return valoresPorCategoria;
 	}
 }
--- src/test/java/br/gov/batch/servicos/cadastro/CategoriaBOTest.java
+++ src/test/java/br/gov/batch/servicos/cadastro/CategoriaBOTest.java
@@ -1,23 +1,56 @@
 package br.gov.batch.servicos.cadastro;
 
+import static org.junit.Assert.assertEquals;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.easymock.TestSubject;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 import br.gov.model.cadastro.Categoria;
 
 public class CategoriaBOTest {
-	
-	private CategoriaBO categoriaBO = new CategoriaBO();
+
+	@TestSubject
+	private CategoriaBO categoriaBO;
+
+	@Before
+	public void setup() {
+		categoriaBO = new CategoriaBO();
+	}
+
+	@Test
+	public void calcularValorPorCategoria() {
+		Collection<Categoria> categorias = new ArrayList<Categoria>();
+		Categoria categoria = new Categoria();
+		categoria.setQuantidadeEconomias(5);
+		categorias.add(categoria);
+		
+		categoria = new Categoria();
+		categoria.setQuantidadeEconomias(2);
+		categorias.add(categoria);
+		
+		Collection<BigDecimal> valores = categoriaBO.obterValorPorCategoria(categorias, BigDecimal.valueOf(5.00));
+		
+		BigDecimal valor = valores.iterator().next();
+		
+		assertEquals(BigDecimal.valueOf(3.58), valor);
+	}
 	
 	@Test
-	public void calculaValorSemCategorias(){
-		Collection<Categoria> lista = new ArrayList<Categoria>();
-		Collection<BigDecimal> valor = categoriaBO.obterValorPorCategoria(lista, BigDecimal.ZERO);
-		
-		assertEquals(0, valor.size());
+	public void calcularValorSemCategorias() {
+		Collection<BigDecimal> valores = categoriaBO.obterValorPorCategoria(new ArrayList<Categoria>(), BigDecimal.ZERO);
+
+		assertEquals(0, valores.size());
+	}
+	
+	@Test
+	public void calcularValorComListaNula() {
+		Collection<BigDecimal> valores = categoriaBO.obterValorPorCategoria(null, BigDecimal.ZERO);
+
+		assertEquals(0, valores.size());
 	}
 }
