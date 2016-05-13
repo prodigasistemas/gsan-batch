20e9067caa54a553143a44b1fb1423c8d1c82ace
Marcelo de Brito Garcia <mbgarcia_pa@yahoo.com.br>
2016-05-13 16:06:13 -0300

    ContaCategoriaBOTest nao usa mock

--- src/test/java/br/gov/batch/servicos/faturamento/ContaCategoriaBOTest.java
+++ src/test/java/br/gov/batch/servicos/faturamento/ContaCategoriaBOTest.java
@@ -5,19 +5,14 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import org.easymock.EasyMockRunner;
-import org.easymock.TestSubject;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import br.gov.model.cadastro.ICategoria;
 import br.gov.servicos.to.ImovelSubcategoriaTO;
 
-@RunWith(EasyMockRunner.class)
 public class ContaCategoriaBOTest {
 
-	@TestSubject
 	private ContaCategoriaBO contaCategoriaBO;
 	
 	@Before
