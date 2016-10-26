2ceb0406bc096055ea8082c3f76b872d7585b57e
Pamela Gatinho <pamelagatinho@gmail.com>
2016-10-26 11:28:23 -0300

    Criando outros testes para validar o calculo de excesso por economia

--- src/test/java/br/gov/batch/servicos/faturamento/tarifa/ConsumoImovelBOTest.java
+++ src/test/java/br/gov/batch/servicos/faturamento/tarifa/ConsumoImovelBOTest.java
@@ -24,12 +24,13 @@ import br.gov.model.micromedicao.ConsumoHistorico;
 @RunWith(MockitoJUnitRunner.class)
 public class ConsumoImovelBOTest {
 
-	@Mock private ConsumoHistorico consumoHistorico;
-	@Mock private Imovel imovel;
-	@Mock private EconomiasBO economiasBO;
-	@Mock private ConsumoBO consumoBO;
-	@Mock private ConsumoTarifaVigencia consumoTarifaVigencia;
-	@Mock private ICategoria categoria;
+	@Mock private ConsumoHistorico consumoHistoricoMock;
+	@Mock private Imovel imovelMock;
+	@Mock private EconomiasBO economiasBOMock;
+	@Mock private ConsumoBO consumoBOMock;
+	@Mock private ConsumoTarifaVigencia consumoTarifaVigenciaMock;
+	@Mock private ICategoria categoriaResidencialMock;
+	@Mock private ICategoria categoriaComercialMock;
 	
 	@InjectMocks private ConsumoImovelBO bo;
 	
@@ -39,29 +40,99 @@ public class ConsumoImovelBOTest {
 		
 		MockitoAnnotations.initMocks(this);
 		
-		when(consumoHistorico.getImovel()).thenReturn(imovel);
-		when(consumoHistorico.getNumeroConsumoFaturadoMes()).thenReturn(24);
-		when(consumoBO.consumoMinimoLigacao(imovel.getId())).thenReturn(10);
+		when(consumoBOMock.getConsumoMinimoTarifaPorCategoria(consumoTarifaVigenciaMock.getId(), categoriaResidencialMock)).thenReturn(10);
+		when(consumoBOMock.getConsumoMinimoTarifaPorCategoria(consumoTarifaVigenciaMock.getId(), categoriaComercialMock)).thenReturn(10);
+		when(consumoHistoricoMock.getImovel()).thenReturn(imovelMock);
 	}
 	
 	@Test
 	public void calculoExcessoImovelPositivo() {
-		int resultado = bo.calculoExcessoImovel(consumoHistorico);
+		when(consumoBOMock.consumoMinimoLigacao(imovelMock.getId())).thenReturn(10);
+		when(consumoHistoricoMock.getNumeroConsumoFaturadoMes()).thenReturn(24);
+		int resultado = bo.calculoExcessoImovel(consumoHistoricoMock);
 		
 		assertEquals(14, resultado);
 	}
 	
 	@Test
 	public void addConsumoImovelTO() {
-		when(economiasBO.getQuantidadeTotalEconomias(imovel.getId())).thenReturn(4);
-		when(consumoBO.getConsumoMinimoTarifaPorCategoria(consumoTarifaVigencia.getId(), categoria)).thenReturn(10);
+		when(economiasBOMock.getQuantidadeTotalEconomias(imovelMock.getId())).thenReturn(4);
+		when(consumoBOMock.consumoMinimoLigacao(imovelMock.getId())).thenReturn(10);
+		when(consumoHistoricoMock.getNumeroConsumoFaturadoMes()).thenReturn(24);
+
 		
-		bo.addConsumoImovelTO(consumoHistorico, consumoTarifaVigencia, categoria);
+		bo.addConsumoImovelTO(consumoHistoricoMock, consumoTarifaVigenciaMock, categoriaResidencialMock);
 		List<ConsumoImovelTO> list = bo.getConsumoImoveisTO();
 		
 		assertEquals(1, list.size());
 		assertEquals(new Integer(10), list.get(0).getConsumoEconomiaCategoria());
 		assertEquals(new Integer(3), list.get(0).getConsumoExcedenteCategoria());
+	}
+	
+	@Test
+	public void addConsumoImovelTOUmaCategoriaUmaEconomia() {
+		when(economiasBOMock.getQuantidadeTotalEconomias(imovelMock.getId())).thenReturn(1);
+		when(consumoBOMock.consumoMinimoLigacao(imovelMock.getId())).thenReturn(10);
+		when(consumoHistoricoMock.getNumeroConsumoFaturadoMes()).thenReturn(40);
+		
+		bo.addConsumoImovelTO(consumoHistoricoMock, consumoTarifaVigenciaMock, categoriaResidencialMock);
+		List<ConsumoImovelTO> list = bo.getConsumoImoveisTO();
+		
+		assertEquals(1, list.size());
+		assertEquals(new Integer(10), list.get(0).getConsumoEconomiaCategoria());
+		assertEquals(new Integer(30), list.get(0).getConsumoExcedenteCategoria());
+	}
+	
+	@Test
+	public void addConsumoImovelTOUmaCategoriaTresEconomias() {
+		when(economiasBOMock.getQuantidadeTotalEconomias(imovelMock.getId())).thenReturn(3);
+		when(consumoBOMock.consumoMinimoLigacao(imovelMock.getId())).thenReturn(30);
+		when(consumoHistoricoMock.getNumeroConsumoFaturadoMes()).thenReturn(48);
+		
+		bo.addConsumoImovelTO(consumoHistoricoMock, consumoTarifaVigenciaMock, categoriaResidencialMock);
+		List<ConsumoImovelTO> list = bo.getConsumoImoveisTO();
+		
+		assertEquals(1, list.size());
+		assertEquals(new Integer(10), list.get(0).getConsumoEconomiaCategoria());
+		assertEquals(new Integer(6), list.get(0).getConsumoExcedenteCategoria());
+	}
+	
+	@Test
+	public void addConsumoImovelTODuasCategoriasDuasEconomias() {
+		when(economiasBOMock.getQuantidadeTotalEconomias(imovelMock.getId())).thenReturn(2);
+		
+		when(consumoBOMock.consumoMinimoLigacao(imovelMock.getId())).thenReturn(20);
+		when(consumoHistoricoMock.getNumeroConsumoFaturadoMes()).thenReturn(40);
+		
+		bo.addConsumoImovelTO(consumoHistoricoMock, consumoTarifaVigenciaMock, categoriaResidencialMock);
+		bo.addConsumoImovelTO(consumoHistoricoMock, consumoTarifaVigenciaMock, categoriaComercialMock);
+		List<ConsumoImovelTO> list = bo.getConsumoImoveisTO();
+		
+		assertEquals(2, list.size());
+		assertEquals(new Integer(10), list.get(0).getConsumoEconomiaCategoria());
+		assertEquals(new Integer(10), list.get(0).getConsumoExcedenteCategoria());
+		
+		assertEquals(new Integer(10), list.get(1).getConsumoEconomiaCategoria());
+		assertEquals(new Integer(10), list.get(1).getConsumoExcedenteCategoria());
+	}
+	
+	@Test
+	public void addConsumoImovelTODuasCategoriasSeteEconomias() {
+		when(economiasBOMock.getQuantidadeTotalEconomias(imovelMock.getId())).thenReturn(7);
+		
+		when(consumoBOMock.consumoMinimoLigacao(imovelMock.getId())).thenReturn(70);
+		when(consumoHistoricoMock.getNumeroConsumoFaturadoMes()).thenReturn(77);
+		
+		bo.addConsumoImovelTO(consumoHistoricoMock, consumoTarifaVigenciaMock, categoriaResidencialMock);
+		bo.addConsumoImovelTO(consumoHistoricoMock, consumoTarifaVigenciaMock, categoriaComercialMock);
+		List<ConsumoImovelTO> list = bo.getConsumoImoveisTO();
+		
+		assertEquals(2, list.size());
+		assertEquals(new Integer(10), list.get(0).getConsumoEconomiaCategoria());
+		assertEquals(new Integer(1), list.get(0).getConsumoExcedenteCategoria());
+		
+		assertEquals(new Integer(10), list.get(1).getConsumoEconomiaCategoria());
+		assertEquals(new Integer(1), list.get(1).getConsumoExcedenteCategoria());
 	}
 
 }
